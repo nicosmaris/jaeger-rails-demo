8c6515b9c3a52fc673c7cf5045b54b460ead2047
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-01-30 10:37:56 +0100

    Add more interaction to trace into second service

--- service-first/app/controllers/ping_pong_controller.rb
+++ service-first/app/controllers/ping_pong_controller.rb
@@ -1,5 +1,6 @@
 class PingPongController < ApplicationController
   def index
+    Article.last
     render plain: 'Ping pong'
   end
 end
--- service-second/app/controllers/home_controller.rb
+++ service-second/app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    Article.last
     conn = Faraday.new(url: 'http://localhost:3000/') do |faraday|
       faraday.use Faraday::Tracer, span: current_span
 
