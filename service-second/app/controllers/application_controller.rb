4249fa12648761f27bc9b49c9895a64a12354fdb
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-01-30 10:37:30 +0100

    Adapt to last caina gem version

--- service-second/app/controllers/application_controller.rb
+++ service-second/app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  include OpentracingRails::Instrumenters::ActiveRecord
-
   protect_from_forgery with: :exception
 end
--- service-second/config.ru
+++ service-second/config.ru
@@ -2,5 +2,4 @@
 
 require_relative 'config/environment'
 
-use Rack::Tracer
 run Rails.application
