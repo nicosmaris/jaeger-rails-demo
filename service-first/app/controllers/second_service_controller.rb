c719cfc453a8b2b64d4639c0d2a3e554614b6354
Nikos Maris <nmaris@crypteianetworks.com>
2018-12-14 17:29:17 +0200

    add custom faraday tracer as a middleware at the right order

--- service-first/app/controllers/second_service_controller.rb
+++ service-first/app/controllers/second_service_controller.rb
@@ -1,15 +1,12 @@
 require 'faraday'
-#require 'faraday/tracer'
 
 class SecondServiceController < ApplicationController
   def index
     # doing an external API call just to track it
     conn = Faraday.new(url: 'http://127.0.0.1:3001/') do |faraday|
-    #  faraday.use Faraday::Tracer, span: request.env['rack.span']
-
+      faraday.use FaradayInjectTracingMiddleware
       faraday.request :url_encoded
       faraday.adapter Faraday.default_adapter
-      faraday.use FaradayInjectTracingMiddleware
     end
 
     # doing a DB request just to track it
